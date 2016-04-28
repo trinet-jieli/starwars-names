b57abad1654255a5b34565e082dd263bb42ecbec
Jie LI <Jie.LI@trinet.com>
2016-04-28 06:29:24 -0400

    implement true unit testing

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,27 @@ var expect = require('chai').expect;
 var starWarsNames = require('./index');
 
 describe('starwars-names', function() {
-  it('should work!', function() {
-    expect(starWarsNames.all.length).to.equal(12);
+
+  describe('all', function() {
+    it('should be an array of strings', function() {
+      expect(starWarsNames.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string';
+        })
+      }
+    });
+
+    it('should have `Zuckuss`', function() {
+      expect(starWarsNames.all).to.include('Zuckuss');
+    });
+  });
+
+  describe('random', function() {
+    it('should return a random name from the starWarsNames.all', function() {
+      var randomItem = starWarsNames.random();
+      expect(starWarsNames.all).to.include(randomItem);
+    });
   });
 });
